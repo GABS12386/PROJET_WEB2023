ac552aa370e9c822dfd4b2b1d0d463c03ec07020
Gabriel <gabriel.randrianaridera@etu.univ-poitiers.fr>
2023-03-31 17:22:48 +0200

    creation des 3 entité v1

---
+++ src/Entity/Panier.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PanierRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PanierRepository::class)]
+class Panier
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\OneToMany(mappedBy: 'panier', targetEntity: Produit::class)]
+    private Collection $idproduit;
+
+    public function __construct()
+    {
+        $this->idproduit = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return Collection<int, Produit>
+     */
+    public function getIdproduit(): Collection
+    {
+        return $this->idproduit;
+    }
+
+    public function addIdproduit(Produit $idproduit): self
+    {
+        if (!$this->idproduit->contains($idproduit)) {
+            $this->idproduit->add($idproduit);
+            $idproduit->setPanier($this);
+        }
+
+        return $this;
+    }
+
+    public function removeIdproduit(Produit $idproduit): self
+    {
+        if ($this->idproduit->removeElement($idproduit)) {
+            // set the owning side to null (unless already changed)
+            if ($idproduit->getPanier() === $this) {
+                $idproduit->setPanier(null);
+            }
+        }
+
+        return $this;
+    }
+}
---
+++ src/Entity/Produit.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProduitRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProduitRepository::class)]
+class Produit
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $libelle = null;
+
+    #[ORM\Column]
+    private ?float $prix = null;
+
+    #[ORM\Column]
+    private ?int $quantite = null;
+
+    #[ORM\ManyToOne(inversedBy: 'idproduit')]
+    private ?Panier $panier = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLibelle(): ?string
+    {
+        return $this->libelle;
+    }
+
+    public function setLibelle(string $libelle): self
+    {
+        $this->libelle = $libelle;
+
+        return $this;
+    }
+
+    public function getPrix(): ?float
+    {
+        return $this->prix;
+    }
+
+    public function setPrix(float $prix): self
+    {
+        $this->prix = $prix;
+
+        return $this;
+    }
+
+    public function getQuantite(): ?int
+    {
+        return $this->quantite;
+    }
+
+    public function setQuantite(int $quantite): self
+    {
+        $this->quantite = $quantite;
+
+        return $this;
+    }
+
+    public function getPanier(): ?Panier
+    {
+        return $this->panier;
+    }
+
+    public function setPanier(?Panier $panier): self
+    {
+        $this->panier = $panier;
+
+        return $this;
+    }
+}
