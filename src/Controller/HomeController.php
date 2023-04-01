341249db484833ff9003ac50b0a2313174888e26
Gabriel <gabriel.randrianaridera@etu.univ-poitiers.fr>
2023-04-01 12:38:03 +0200

    HomeController + 1 twig

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'accueil_index')]
+    public function indexAction(): Response
+    {
+        return $this->render('/indew.html.twig');
+    }
+
+    /*public function menuAction(): Response
+    {
+        $args = array(
+        );
+        return $this->render('/menu.html.twig', $args);
+    }*/
+
+
+
+}
+
