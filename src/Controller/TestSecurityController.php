6799b6e761f2493150bc1376e89af72ffc9db465
Gabriel <gabriel.randrianaridera@etu.univ-poitiers.fr>
2023-04-17 16:57:05 +0200

    Securite V1

---
+++ src/Controller/TestSecurityController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+use Symfony\Component\Routing\Annotation\Route;
+
+class TestSecurityController extends AbstractController
+{
+    #[Route('/test/security', name: 'app_test_security')]
+    public function index(EntityManagerInterface $em , UserPasswordHasherInterface $passwordHasher): Response
+    {
+        $user1 = new User();
+        $user1 ->setLogin("sadmin")
+                ->setNom("Sadmin")
+                ->setBirthday( new \DateTimeImmutable("02/04/1950"))
+                ->setRoles(["ROLE_SUPER_ADMIN"])
+                ->setPrenom("Sadmin");
+            $hashedPassword = $passwordHasher->hashPassword($user1, 'nimdas');
+            $user1->setPassword($hashedPassword);
+
+
+
+       /* $user2 = new User();
+        $user2 ->setNom("sadmin")
+            ->setPrenom("gilles");
+        $hashedPassword = $passwordHasher->hashPassword($user1, 'nimdas');
+        $user1->setPassword($hashedPassword);
+
+        $em->persist($user1);
+
+        $user1 = new User();
+        $user1 ->setNom("sadmin")
+            ->setPrenom("gilles");
+        $hashedPassword = $passwordHasher->hashPassword($user1, 'nimdas');
+        $user1->setPassword($hashedPassword);
+
+        $em->persist($user1);
+
+        $user1 = new User();
+        $user1 ->setNom("sadmin")
+            ->setPrenom("gilles");
+        $hashedPassword = $passwordHasher->hashPassword($user1, 'nimdas');
+        $user1->setPassword($hashedPassword);*/
+
+        $em->persist($user1);
+        $em->flush();
+
+
+       return $this->redirectToRoute("accueil_index");
+    }
+}
