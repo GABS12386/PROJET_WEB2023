ca92f27a87b722cd740384880fb152248b9070f1
Gabriel <gabriel.randrianaridera@etu.univ-poitiers.fr>
2023-04-18 14:07:01 +0200

    securite creation des 4 utilisateurs a tester

--- src/Controller/TestSecurityController.php
+++ src/Controller/TestSecurityController.php
@@ -16,38 +16,49 @@ class TestSecurityController extends AbstractController
     {
         $user1 = new User();
         $user1 ->setLogin("sadmin")
-                ->setNom("Sadmin")
+                ->setNom("Jean")
                 ->setBirthday( new \DateTimeImmutable("02/04/1950"))
                 ->setRoles(["ROLE_SUPER_ADMIN"])
-                ->setPrenom("Sadmin");
+                ->setPrenom("Albert");
             $hashedPassword = $passwordHasher->hashPassword($user1, 'nimdas');
             $user1->setPassword($hashedPassword);
 
+        $em->persist($user1);
 
 
         $user2 = new User();
-        $user2 ->setNom("sadmin")
-            ->setPrenom("gilles");
-        $hashedPassword = $passwordHasher->hashPassword($user1, 'nimdas');
-        $user1->setPassword($hashedPassword);
+        $user2 ->setLogin("gilles")
+                ->setNom("Subrenat")
+                ->setBirthday(new \DateTimeImmutable("05/06/1958"))
+                ->setRoles(["ROLE_ADMIN"])
+                ->setPrenom("Gilles");
+            $hashedPassword2 = $passwordHasher ->hashPassword($user2,'sellig');
+            $user2->setPassword($hashedPassword2);
 
-        $em->persist($user1);
+        $em->persist($user2);
 
-        $user1 = new User();
-        $user1 ->setNom("sadmin")
-            ->setPrenom("gilles");
-        $hashedPassword = $passwordHasher->hashPassword($user1, 'nimdas');
-        $user1->setPassword($hashedPassword);
+        $user3 = new User();
+        $user3 ->setLogin("rita")
+                ->setNom("Zrour")
+                ->setBirthday(new \DateTimeImmutable("14/09/1965"))
+                ->setRole(["ROLE_USER"])
+                ->setPrenom("Rita");
+            $hashedPassword3 = $passwordHasher ->hashPassword($user3,'atir');
+            $user3->setPassword($hashedPassword3);
 
-        $em->persist($user1);
+        $em->persist($user3);
 
-        $user1 = new User();
-        $user1 ->setNom("sadmin")
-            ->setPrenom("gilles");
-        $hashedPassword = $passwordHasher->hashPassword($user1, 'nimdas');
-        $user1->setPassword($hashedPassword);*/
+        $user4 = new User();
+        $user4 ->setLogin("simon")
+                ->setNom("Rame")
+                ->setBirthday(new \DateTimeImmutable("23/12/1982"))
+                ->setRoles(["ROLE_USER"])
+                ->setPrenom("Simon");
+            $hashedPassword4 = $passwordHasher ->hashPassword($user4,'nomis');
+            $user4->setPassword($hashedPassword4);
 
-        $em->persist($user1);
+        $em->persist($user4);
+
         $em->flush();
 
 
